6830bb6c793741d2a9af26c458097afcf05cac45
Kalssy <900noksusu@naver.com>
2024-12-08 18:45:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+import openai
 from langchain_openai import ChatOpenAI
-chat_model = ChatOpenAI()
-
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=openai.api_key)
 
 st.title("인공지능 시인")
 subject = st.text_input("시의 주제를 입력해주세요.")
@@ -9,5 +16,5 @@ st.write("시의 주제 : " + subject)
 
 if st.button("시 작성"):
     with st.spinner("시 작성중 ..."):
-        result = chat_model.invoke(subject + "에 대한 시를 써줘")
-        st.write(result.content)
+        response = chat_model(messages=[{"role": "user", "content": f"{subject}에 대한 시를 써줘"}])
+        st.write(response.content)
